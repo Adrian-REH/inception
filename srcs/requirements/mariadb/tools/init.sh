d44a0e4c9e0eaf9553ec94d17dc4ebf1f43582d9
Adrian Herrera <64231248+Adrian-REH@users.noreply.github.com>
2024-12-25 15:41:30 +0100

    Init mariadb with init.sql, added script .sh and allowed host "0.0.0.0"

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-
+set -eo pipefail
 
 # Expandir variables de entorno en el archivo .sql
-envsubst < ./docker-entrypoint-initdb.d/init.sql > ./docker-entrypoint-initdb.d/init.sql
+envsubst < /init.sql > /tmp/init_expanded.sql
+#echo "Archivo SQL con variables de entorno expandido:"
+#cat /tmp/init_expanded.sql
 
 
-echo "Initializing MariaDB..."
+
+
+# Si el directorio de datos está vacío, inicializa MariaDB
+if [ ! -d "$MYSQL_DATA_DIR/mysql" ]; then
+    mysql_install_db --user=mysql --datadir="$MYSQL_DATA_DIR"
+fi
+
+# Inicia MariaDB en segundo plano
+/usr/bin/mysqld_safe --datadir="$MYSQL_DATA_DIR" &
+
+# Espera a que MariaDB esté listo
+until mysqladmin ping >/dev/null 2>&1; do
+  echo -n "."; sleep 0.2
+done
+
+
+mysql < /tmp/init_expanded.sql
+
+# Detiene MariaDB
+mysqladmin shutdown
+
+# Inicia MariaDB en primer plano
+exec mysqld --user=mysql --console
