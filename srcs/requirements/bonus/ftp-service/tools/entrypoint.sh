793852ba0e030a2cb812a330907181aaae2a663a
Adrian Herrera <64231248+Adrian-REH@users.noreply.github.com>
2024-12-27 21:53:03 +0100

    added ftp

---
+++ srcs/requirements/bonus/ftp-service/tools/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Verificar si el archivo de respaldo de configuración no existe
+if [ ! -f "/etc/vsftpd/vsftpd.conf.bak" ]; then
+
+    # Crear el directorio de WordPress si no existe
+    mkdir -p /var/www/html
+
+    # Hacer una copia de seguridad del archivo de configuración y mover el nuevo archivo
+    cp /etc/vsftpd.conf /etc/vsftpd/vsftpd.conf.bak
+    mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
+
+    # Crear el usuario FTP, configurar la contraseña y dar permisos sobre el directorio de WordPress
+    adduser $FTP_USR --disabled-password --gecos ""  # --gecos para evitar una pregunta interactiva
+    echo "$FTP_USR:$FTP_PWD" | /usr/sbin/chpasswd &> /dev/null
+    chown -R $FTP_USR:$FTP_USR /var/www/html
+
+    # Añadir el usuario a la lista de usuarios de vsftpd
+    echo $FTP_USR | tee -a /etc/vsftpd.userlist &> /dev/null
+fi
+
+# Iniciar el servicio FTP
+echo "FTP started on :21"
+/usr/sbin/vsftpd /etc/vsftpd.conf
